7d168e58c6d4525aa747cd68cc286a98678cc239
Alberto Ruiz <aruiz@gnome.org>
2014-05-07 15:08:55 +0200

    Add JS and CSS files and HTTP methods

--- server.py
+++ server.py
@@ -14,10 +14,22 @@ def profiles(profile_id):
   return send_from_directory(os.path.join(os.getcwd(), "profiles"), profile_id)
 
 @app.route("/submit_change", methods=["POST"])
-def hello():
+def submit_change():
   print(request.data)
   session.append(data)
   return "{\"status\": \"ok\"}"
+
+@app.route("/js/<path:js>", methods=["GET"])
+def js_files(js):
+  return send_from_directory(os.path.join(os.getcwd(), "js"), js)
+
+@app.route("/css/<path:js>", methods=["GET"])
+def css_files(js):
+  return send_from_directory(os.path.join(os.getcwd(), "css"), js)
+
+@app.route("/", methods=["GET"])
+def index():
+  return "Index"
 
 if __name__ == "__main__":
       app.run(port=8181)
