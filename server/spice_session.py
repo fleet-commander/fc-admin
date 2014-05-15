f061dc7b5f1c5a331bfa65fe9fd789d5f505be10
Alberto Ruiz <aruiz@gnome.org>
2014-05-15 18:53:24 +0200

    Add websockify to the fc-user session

--- server/spice_session.py
+++ server/spice_session.py
@@ -10,22 +10,27 @@ class SpiceSessionManager:
   XSPICE = 'Xspice :10 --disable-ticketing --port 8280'
   GNOME_SESSION = 'DISPLAY=:10 gnome-session'
   GSETTINGS_LOGGER = 'DISPLAY=:10 fc-gsettings-logger'
+  WEBSOCKIFY = 'websockify localhost:8281 localhost:8280'
 
   def __init__(self):
     self.xspice = None
     self.gnome_session = None
     self.gsettings_logger = None
+    self.websockify = None
 
   def start(self):
-    if self.xspice or self.gnome_session or self.gsettings_logger:
+    if self.xspice or self.gnome_session or self.gsettings_logger or self.websockify:
       return False
 
     DNULL = open('/dev/null', 'w')
     template = "su -c \"%s\" - fc-user"
     self.xspice = subprocess.Popen(template % self.XSPICE, shell=True, stdout=DNULL, stderr=DNULL, stdin=DNULL)
+    
     time.sleep(1)
-    self.gnome_session = subprocess.Popen(template % self.GNOME_SESSION, shell=True, stdout=DNULL, stderr=DNULL, stdin=DNULL)
+
+    self.gnome_session    = subprocess.Popen(template % self.GNOME_SESSION,    shell=True, stdin=DNULL, stdout=DNULL, stderr=DNULL)
     self.gsettings_logger = subprocess.Popen(template % self.GSETTINGS_LOGGER, shell=True, stdin=DNULL, stdout=DNULL, stderr=DNULL)
+    self.websockify       = subprocess.Popen(template % self.WEBSOCKIFY,       shell=True, stdin=DNULL, stdout=DNULL, stderr=DNULL)
     return True
 
   def stop(self):
@@ -33,24 +38,10 @@ class SpiceSessionManager:
     subprocess.call ('pkill -u fc-user', shell=True)
     subprocess.call ('pkill -f "Xorg -config spiceqxl.xorg.conf -noreset :10"', shell=True)
 
-  def alt_stop(self):
-    if self.gsettings_logger:
-      self.gsettings_logger.terminate()
-    if self.gnome_session:
-      self.gnome_session.terminate()
-    if self.xspice:
-      self.xspice.terminate()
-    
-    if self.gsettings_logger and self.gsettings_logger.poll() == None:
-      self.gsettings_logger.kill()
-    if self.gnome_session and self.gnome_session.poll() == None:
-      self.gnome_session.kill()
-    if self.xspice and self.xspice.poll() == None:
-      self.xspice.kill()
-
     self.xspice = None
     self.gnome_session = None
     self.gsettings_logger = None
+    self.websockify
 
 app = Flask(__name__)
 
