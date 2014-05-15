54475d4e3a866e7fb7363b84d7e1d9279c79f061
Alberto Ruiz <aruiz@gnome.org>
2014-05-15 17:54:49 +0200

    Rename spice-session.py to spice_session.py for importability

--- server/spice_session.py
+++ server/spice_session.py
@@ -2,34 +2,38 @@ import subprocess
 import os
 import time
 import signal
+import uuid
+
+from flask import Flask
 
 class SpiceSessionManager:
-  XSPICE = 'Xspice :10 --disable-ticketing --port 8182'
+  XSPICE = 'Xspice :10 --disable-ticketing --port 8280'
   GNOME_SESSION = 'DISPLAY=:10 gnome-session'
-  #GNOME_SHELL = 'gnome-shell'
-  GSETTINGS_LOGGER = 'fc-gsettings-logger'
+  GSETTINGS_LOGGER = 'DISPLAY=:10 fc-gsettings-logger'
 
   def __init__(self):
-    self.env = os.environ.copy()
-    self.env['DISPLAY'] = ':10'
     self.xspice = None
     self.gnome_session = None
-    self.gnome_shell = None
     self.gsettings_logger = None
 
   def start(self):
+    if self.xspice or self.gnome_session or self.gsettings_logger:
+      return False
+
     DNULL = open('/dev/null', 'w')
     template = "su -c \"%s\" - fc-user"
     self.xspice = subprocess.Popen(template % self.XSPICE, shell=True, stdout=DNULL, stderr=DNULL, stdin=DNULL)
     time.sleep(1)
     self.gnome_session = subprocess.Popen(template % self.GNOME_SESSION, shell=True, stdout=DNULL, stderr=DNULL, stdin=DNULL)
-    #self.gnome_shell = subprocess.Popen(template % self.GNOME_SHELL, shell=True)
     self.gsettings_logger = subprocess.Popen(template % self.GSETTINGS_LOGGER, shell=True, stdin=DNULL, stdout=DNULL, stderr=DNULL)
+    return True
 
   def stop(self):
-    if not self.gsettings_logger and not self.gnome_session and not self.xspice:
-      return
+    #NOITE: This is a brute force approach to kill all fc-user processes
+    subprocess.call ('pkill -u fc-user', shell=True)
+    subprocess.call ('pkill -f "Xorg -config spiceqxl.xorg.conf -noreset :10"', shell=True)
 
+  def alt_stop(self):
     if self.gsettings_logger:
       self.gsettings_logger.terminate()
     if self.gnome_session:
@@ -44,14 +48,40 @@ class SpiceSessionManager:
     if self.xspice and self.xspice.poll() == None:
       self.xspice.kill()
 
-if __name__ == '__main__':
-  session = SpiceSessionManager()
-  session.start()
+    self.xspice = None
+    self.gnome_session = None
+    self.gsettings_logger = None
 
-  def sigint_handler(sig, frm):
-    session.stop()
+app = Flask(__name__)
 
-  signal.signal(signal.SIGINT, sigint_handler)
+has_session = False
+spice = SpiceSessionManager()
 
-  signal.pause()
+@app.route("/start_session", methods=["GET"])
+def new_session():
+  global has_session
+  global spice
 
+  if has_session:
+    return '{"status": "already_started"}', 403
+
+  spice.start()
+
+  has_session = True
+  return '{"status": "ok"}', 200
+
+@app.route("/stop_session")
+def stop_session():
+  global has_session
+  global spice
+
+  if not has_session:
+    return '{"status": "already_stopped"}', 403
+
+  spice.stop()
+
+  has_session = False
+  return '{"status": "stopped"}', 200
+
+if __name__ == '__main__':  
+  app.run(host='localhost', port=8182, debug=True)
