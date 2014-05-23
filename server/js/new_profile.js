88e0bf636d5fc978b5caa6390bc0c24e4075a7bd
Alberto Ruiz <aruiz@gnome.org>
2014-05-23 18:13:49 +0200

    Add several buttons to the profile editor to hide/show session/changelog

--- server/js/new_profile.js
+++ server/js/new_profile.js
@@ -1,11 +1,14 @@
 var sc;
+var updater;
 
 function updateEventList () {
   $.getJSON ("/session_changes", function (data) {
     $("#event-list").html("");
     $.each (data, function (i, item) {
       var row = item.join (" ");
-      $("#event-list").html($("#event-list").html() + "<li>" + row + "</li>");
+      var id = data.length - 1 - i;
+      var input = '<input type="checkbox" class="hidden" data-id="' + id + '"/>'
+      $("#event-list").html($("#event-list").html() + "<li>" + input + row + "</li>");
     });
   });
 }
@@ -23,10 +26,34 @@ function startSpice () {
   }
 }
 
+function reviewChanges() {
+  $("#spice-area").hide(200);
+  $("#event-logs").show(200);
+}
+
+function showSession() {
+  $("#spice-area").show(200);
+  $("#event-logs").hide(200);
+}
+
+function createProfile() {
+  closeSession();
+  $('input[type="button"]').hide();
+  $("input.hidden").css("display", "inline");
+  window.clearInterval(updater);
+  window.setTimeout(function () {
+    $("input.hidden").css("display", "inline");
+    reviewChanges();
+  },
+    1000);
+}
+
 $(document).ready (function () {
-  window.setInterval (updateEventList, 1000);
+  updater = window.setInterval (updateEventList, 1000);
 
   $.getJSON("/session_start", function (data) {
     window.setTimeout(startSpice, 1000);
   });
+
+  $("#event-logs").hide();
 });
