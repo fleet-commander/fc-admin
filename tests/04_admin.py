eb57e46096a143d7d1fb1d0837268c6d6792808c
Alberto Ruiz <aruiz@redhat.com>
2015-07-08 20:23:27 +0100

    tests: admin: add tests to check for session start/stop methods

--- tests/04_admin.py
+++ tests/04_admin.py
@@ -25,20 +25,56 @@ import unittest
 import shutil
 import urllib
 import tempfile
+import requests
 
+class MockResponse:
+  pass
 
 class MockRequests:
+  DEFAULT_CONTENT = "SOMECONTENT"
+  DEFAULT_STATUS  = 200
+  exceptions = requests.exceptions
   def __init__(self):
     self.queue = []
+    self.raise_error = False
+    self.set_default_values()
+
   def get(self, url):
+    if self.raise_error:
+      raise requests.exceptions.ConnectionError('Connection failed!')
     self.queue.append(url)
+    ret = MockRequests()
+    ret.content = self.content
+    ret.status_code = self.status_code
+    return ret
+
   def pop(self):
     return self.queue.pop()
+
+  def set_default_values(self):
+    self.content = self.DEFAULT_CONTENT
+    self.status_code = self.DEFAULT_STATUS
+    self.raise_error = False
+
+  def raise_error_on_get(self):
+    self.raise_error = True
+
+class MockVncWebSocket:
+  def __init__(self, **kwargs):
+    self.started = False
+    self.args = kwargs
+  def start(self):
+    self.started = True
+  def stop(self):
+    self.started = False
 
 PYTHONPATH = os.path.join(os.environ['TOPSRCDIR'], 'admin')
 sys.path.append(PYTHONPATH)
 import fleet_commander_admin
+
+# assigned mocked objects
 fleet_commander_admin.requests = MockRequests()
+fleet_commander_admin.VNC_WSOCKET = MockVncWebSocket()
 
 class TestAdmin(unittest.TestCase):
   cookie = "/tmp/fleet-commander-start"
@@ -62,6 +98,7 @@ class TestAdmin(unittest.TestCase):
   def setUp(self):
     shutil.rmtree(self.args['profiles_dir'])
     self.args['profiles_dir'] = tempfile.mkdtemp()
+    fleet_commander_admin.requests.set_default_values()
 
   def get_data_from_file(self, path):
     return open(path).read()
@@ -77,15 +114,50 @@ class TestAdmin(unittest.TestCase):
     self.assertEqual(ret.data, self.get_data_from_file(INDEX_FILE),
         msg='index content was not correct')
 
-  def test_01_attempt_create_unselected_profile(self):
+  def test_01_attempt_save_unselected_profile(self):
     profile_id = '0123456789'
     profile_data = urllib.urlencode(dict(name='myprofile', description='mydesc', settings={}, groups='', users=''))
-    ret = self.app.post('/profile/save/' + profile_id , data=profile_data, follow_redirects=False)
+    ret = self.app.post('/profile/save/' + profile_id , data=profile_data, content_type='application/x-www-form-urlencode')
 
     PROFILE = os.path.join (self.args['profiles_dir'], profile_id + '.json')
     self.assertFalse(os.path.exists(PROFILE), msg='profile file was not created')
     self.assertEqual (ret.status_code, 403)
     self.assertEqual (json.dumps(json.loads(ret.data)), json.dumps({"status": "nonexistinguid"}))
+
+  def test_02_session_start_stop(self):
+    # Start session
+    host = 'somehost'
+    ret = self.app.post('/session/start', data='host='+host, content_type='application/x-www-form-urlencoded')
+
+    self.assertTrue (fleet_commander_admin.VNC_WSOCKET.started)
+    self.assertEqual (fleet_commander_admin.VNC_WSOCKET.target_host, host)
+    self.assertEqual (fleet_commander_admin.VNC_WSOCKET.target_port, 5935)
+    self.assertEqual (ret.data, MockRequests.DEFAULT_CONTENT)
+    self.assertEqual (ret.status_code, MockRequests.DEFAULT_STATUS)
+    self.assertEqual (fleet_commander_admin.requests.pop(), "http://%s:8182/session/start" % host)
+
+    ret = self.app.post('/session/stop', data='host='+host, content_type='application/x-www-form-urlencoded')
+    self.assertEqual(fleet_commander_admin.requests.pop(), "http://%s:8182/session/stop" % host)
+    self.assertEqual(ret.status_code, 200)
+    self.assertEqual(ret.data, MockRequests.DEFAULT_CONTENT)
+
+  def test_03_stop_start_failed_connection(self):
+    fleet_commander_admin.requests.raise_error_on_get()
+
+    host = 'somehost'
+    rets = [self.app.post('/session/start', data='host='+host, content_type='application/x-www-form-urlencoded'),
+            self.app.post('/session/stop', data='host='+host, content_type='application/x-www-form-urlencoded')]
+
+    for ret in rets:
+      self.assertEqual(json.dumps(json.loads(ret.data)), json.dumps({'status': 'could not connect to host'}))
+      self.assertEqual(ret.status_code, 403)
+
+  def test_04_change_select_and_deploy(self):
+    pass
+#    change = {'key':'/foo/bar', 'schema':'foo', 'value':True, 'signature':'b'}
+#    ret = self.app.post('/submit_change/org.gnome.gsettings', data=json.dumps(change), content_type='application/json')
+#    self.assertEqual(json.dumps({"status": "ok"}), json.dumps(json.loads(ret.data)))
+#    self.assertEqual(ret.status_code, 200)
 
 if __name__ == '__main__':
   unittest.main()
