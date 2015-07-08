8e22fc3c3c0c3f936aa7271e5b0df14392f64f21
Alberto Ruiz <aruiz@redhat.com>
2015-07-08 17:10:05 +0100

    tests: Add initial test suite for admin web service

---
+++ tests/04_admin.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# vi:ts=2 sw=2 sts=2
+
+# Copyright (C) 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the licence, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Alberto Ruiz <aruiz@redhat.com>
+
+import os
+import sys
+import json
+import unittest
+import shutil
+import urllib
+import tempfile
+
+
+class MockRequests:
+  def __init__(self):
+    self.queue = []
+  def get(self, url):
+    self.queue.append(url)
+  def pop(self):
+    return self.queue.pop()
+
+PYTHONPATH = os.path.join(os.environ['TOPSRCDIR'], 'admin')
+sys.path.append(PYTHONPATH)
+import fleet_commander_admin
+fleet_commander_admin.requests = MockRequests()
+
+class TestAdmin(unittest.TestCase):
+  cookie = "/tmp/fleet-commander-start"
+  args = {
+      'host': 'localhost',
+      'port': 8777,
+      'profiles_dir': tempfile.mkdtemp(),
+      'data_dir': PYTHONPATH,
+  }
+
+  @classmethod
+  def setUpClass(cls):
+    fleet_commander_admin.app.custom_args = cls.args
+    fleet_commander_admin.app.config['TESTING'] = True
+    cls.app = fleet_commander_admin.app.test_client()
+
+  @classmethod
+  def tearDownClass(cls):
+    shutil.rmtree(cls.args['profiles_dir'])
+
+  def setUp(self):
+    shutil.rmtree(self.args['profiles_dir'])
+    self.args['profiles_dir'] = tempfile.mkdtemp()
+
+  def get_data_from_file(self, path):
+    return open(path).read()
+
+  def test_00_profiles(self):
+    ret = self.app.get("/profiles/")
+
+    INDEX_FILE = os.path.join (self.args['profiles_dir'], 'index.json')
+
+    self.assertEqual(ret.status_code, 200)
+    self.assertTrue(os.path.exists(INDEX_FILE),
+        msg='index file was not created')
+    self.assertEqual(ret.data, self.get_data_from_file(INDEX_FILE),
+        msg='index content was not correct')
+
+  def test_01_attempt_create_unselected_profile(self):
+    profile_id = '0123456789'
+    profile_data = urllib.urlencode(dict(name='myprofile', description='mydesc', settings={}, groups='', users=''))
+    ret = self.app.post('/profile/save/' + profile_id , data=profile_data, follow_redirects=False)
+
+    PROFILE = os.path.join (self.args['profiles_dir'], profile_id + '.json')
+    self.assertFalse(os.path.exists(PROFILE), msg='profile file was not created')
+    self.assertEqual (ret.status_code, 403)
+    self.assertEqual (json.dumps(json.loads(ret.data)), json.dumps({"status": "nonexistinguid"}))
+
+if __name__ == '__main__':
+  unittest.main()
