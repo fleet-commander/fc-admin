57451e85e4e765a5c08ba68ffe9c656c118f0a5b
Oliver Gutierrez <ogutierrez@redhat.com>
2016-07-12 16:33:40 +0100

    goa, admin: Added initial GOA interface

---
+++ admin/fleetcommander/goa.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# vi:ts=4 sw=4 sts=4
+
+# Copyright (C) 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the licence, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Alberto Ruiz <aruiz@redhat.com>
+#          Oliver Gutiérrez <ogutierrez@redhat.com>
+
+import sys
+import json
+from ConfigParser import RawConfigParser, ParsingError
+import logging
+
+
+class GOAProvidersLoader(object):
+    """
+    GOA providers reader
+    """
+    def __init__(self, providers_file):
+        """
+        Class initialization
+        """
+        self.path = providers_file
+        self.providers = {}
+        self.configparser = RawConfigParser()
+        try:
+            self.configparser.read(providers_file)
+        except IOError, e:
+            logging.error(
+                'Could not find GOA providers file %s' % providers_file)
+            raise e
+        except ParsingError, e:
+            logging.error(
+                'There was an error parsing %s' % providers_file)
+            raise e
+        except Exception, e:
+            logging.error(
+                'There was an unknown error parsing %s' % providers_file)
+            raise e
+        self.read_data()
+
+    def read_data(self):
+        for section in self.configparser.sections():
+            if section.startswith('Provider '):
+                provider = section.split()[1]
+                self.providers[provider] = {
+                    'name': self.generate_readable_name(provider),
+                    'services': {}
+                }
+                for option in self.configparser.options(section):
+                    if option.endswith('enabled'):
+                        service = option[:-7]
+                        name = self.generate_readable_name(service)
+                        enabled = self.configparser.getboolean(section, option)
+                        # Generate readable name
+                        self.providers[provider]['services'][service] = {
+                            'name': name,
+                            'enabled': enabled
+                        }
+
+    def generate_readable_name(self, identifier):
+        """
+        Generates readable names for providers and services
+        """
+        return identifier.title().replace('_', ' ')
+
+    def to_json(self):
+        return json.dumps(self.providers)
+
+if __name__ == '__main__':
+    print(GOAProvidersLoader('../../data/goa-providers.conf').to_json())
