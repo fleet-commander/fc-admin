c03b9ae37695b871c7f8f375b12cf8615055bc6d
Oliver Gutierrez <ogutsua@gmail.com>
2018-03-22 13:18:59 +0000

    autotools, tests, admin: Code cleanup

--- tests/_05_fcdbus_tests.py
+++ tests/_05_fcdbus_tests.py
@@ -106,13 +106,9 @@ class TestDbusService(unittest.TestCase):
         self.test_directory = tempfile.mkdtemp()
 
         self.args = {
-            'profiles_dir': os.path.join(self.test_directory, 'profiles'),
             'database_path': os.path.join(self.test_directory, 'database.db'),
             'tmp_session_destroy_timeout': 60,
         }
-
-        self.INDEX_FILE = os.path.join(self.args['profiles_dir'], 'index.json')
-        self.APPLIES_FILE = os.path.join(self.args['profiles_dir'], 'applies.json')
 
         # Open service
         self.service = subprocess.Popen([
