426a2fb035449568793b0018b304067d361035f8
Oliver Gutierrez <ogutsua@gmail.com>
2018-08-06 17:19:46 +0100

    logger: Fixed license text

--- logger/fleet_commander_logger.py
+++ logger/fleet_commander_logger.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # vi:ts=4 sw=4 sts=4
+
+# Copyright (C) 2012 Red Hat, Inc.
 #
-# Copyright (c) 2015 Red Hat, Inc.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the licence, or (at your option) any later version.
 #
-# GNOME Maps is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 2 of the License, or (at your
-# option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# GNOME Maps is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with GNOME Maps; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
 #
 # Authors: Alberto Ruiz <aruiz@redhat.com>
 #          Matthew Barnes <mbarnes@redhat.com>
