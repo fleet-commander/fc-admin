b65b319f806f64ad7f5328cf199557e62ca25dd0
Stanislav Levin <slev@altlinux.org>
2020-08-21 13:21:14 +0300

    tests: Mock dbus for NetworkManager
    There may be no running system bus or NetworkManager installed.

---
+++ tests/03_logger_nm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# We assume dbus-launch never fails
+
+kill $DBUS_SESSION_BUS_PID > /dev/null 2> /dev/null
+
+eval `dbus-launch`
+export DBUS_SESSION_BUS_ADDRESS
+
+
+$PYTHON $TOPSRCDIR/tests/_03_mock_nm_dbus.py &
+DBUS_MOCK_PID=$!
+
+$PYTHON $TOPSRCDIR/tests/_wait_for_name.py org.freedesktop.NetworkManager
+if [ $? -ne 0 ] ; then
+  echo "Failed to acquire bus name org.freedesktop.NetworkManager"
+  exit 1
+fi
+
+ps -p $DBUS_MOCK_PID > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  echo "Failed to launch _03_mock_nm_dbus.py"
+  exit 1
+fi
+
+# Execute fleet commander NM logger tests
+$PYTHON $TOPSRCDIR/tests/_03_logger_nm.py
+RET=$?
+
+kill $DBUS_SESSION_BUS_PID
+
+rm $TOPSRCDIR/_build/sub/admin/fleetcommander/constants.pyc > /dev/null 2>&1
+
+exit $RET
---
+++ tests/_03_mock_nm_dbus.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+
+import logging
+
+import dbus.service
+import dbusmock
+import dbus.mainloop.glib
+from gi.repository import GLib
+
+# Set logging level to debug
+log = logging.getLogger()
+level = logging.getLevelName('DEBUG')
+log.setLevel(level)
+
+ml = GLib.MainLoop()
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+bus = dbusmock.testcase.DBusTestCase.get_dbus()
+
+bus.add_signal_receiver(
+    ml.quit,
+    signal_name='Disconnected',
+    path='/org/freedesktop/DBus/Local',
+    dbus_interface='org.freedesktop.DBus.Local')
+
+nm_bus = dbus.service.BusName(
+    'org.freedesktop.NetworkManager',
+    bus,
+    allow_replacement=True,
+    replace_existing=True,
+    do_not_queue=True)
+
+logging.debug('Configured and running NetworkManager dbus mock')
+
+ml.run()
+
+logging.debug('Quitting NetworkManager dbus mock')
