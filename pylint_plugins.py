deb9170020d6b5ff3c29c8fd3d91b0630ebfed46
Stanislav Levin <slev@altlinux.org>
2020-08-25 14:04:03 +0300

    pylint: Teach about FreeIPA API

---
+++ pylint_plugins.py
@@ -0,0 +1,22 @@
+""" Plugin to teach Pylint about FreeIPA API
+"""
+
+import textwrap
+
+from astroid import MANAGER
+from astroid.builder import AstroidBuilder
+
+
+def register(linter):
+    pass
+
+
+AstroidBuilder(MANAGER).string_build(textwrap.dedent(
+    """
+    from ipalib import api
+    from ipalib import plugable
+
+    api.Backend = plugable.APINameSpace(api, None)
+    api.Command = plugable.APINameSpace(api, None)
+    """
+))
