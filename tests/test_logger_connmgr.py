a84313489d2c73d335605c6b45d8bbb8d2d4667a
Stanislav Levin <slev@altlinux.org>
2020-11-19 10:49:24 +0300

    Adapt logger_connmgr tests
    Signed-off-by: Stanislav Levin <slev@altlinux.org>

--- tests/test_logger_connmgr.py
+++ tests/test_logger_connmgr.py
@@ -22,12 +22,18 @@
 
 # Python imports
 from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
-import sys
-import os
+
 import json
 import logging
+import math
+import os
+import re
+import sys
 import unittest
+
+from unittest.mock import mock_open, patch
 
 # GObject Introspection imports
 from gi.repository import Gio
@@ -35,6 +41,11 @@ from gi.repository import Gio
 PYTHONPATH = os.path.join(os.environ["TOPSRCDIR"], "logger")
 sys.path.append(PYTHONPATH)
 
+from fleet_commander_logger import (
+    FC_LOGGER_PROTO_CHUNK_SIZE,
+    FC_LOGGER_PROTO_HEADER,
+    FC_LOGGER_PROTO_SUFFIX,
+)
 import fleet_commander_logger as FleetCommander
 
 # Set logging level to debug
@@ -44,23 +55,39 @@ log.setLevel(level)
 
 
 def read_file(filename):
-    with open(filename, "r") as fd:
-        data = fd.read()
-        fd.close()
-    return data
+    with open(filename) as fd:
+        return fd.read()
+
+
+def parse_fc_message(msg):
+    """Parser for completely received message of FleetCommander Logger.
+
+    Return list of JSON objects.
+    """
+    m = re.match(rf"^{FC_LOGGER_PROTO_HEADER}(.*){FC_LOGGER_PROTO_SUFFIX}$", msg)
+    return m.group(1).split(FC_LOGGER_PROTO_SUFFIX)
 
 
 class TestConnMgr(unittest.TestCase):
-    def setUp(self):
-        pass
+    """Test SpicePortManager."""
 
-    def test_01_submit_change(self):
+    def test_01_proto_version(self):
+        """Test SpicePortManager sends logger protocol version on instantiation."""
+        TMPFILE = Gio.file_new_tmp("fc_logger_spiceport_XXXXXX")
+        path = TMPFILE[0].get_path()
+        FleetCommander.SpicePortManager(path)
+
+        # Check PROTO header has been written to spiceport file
+        raw_data = read_file(path)
+        self.assertEqual(raw_data, FC_LOGGER_PROTO_HEADER)
+
+    def test_02_submit_change(self):
         # Get temporary file
         TMPFILE = Gio.file_new_tmp("fc_logger_spiceport_XXXXXX")
         path = TMPFILE[0].get_path()
         mgr = FleetCommander.SpicePortManager(path)
         PAYLOAD = '["PAYLOAD"]'
-        expected_data = '{"ns": "org.gnome.gsettings", "data": "[\\"PAYLOAD\\"]"}'
+        expected_data = ['{"ns": "org.gnome.gsettings", "data": "[\\"PAYLOAD\\"]"}']
         mgr.submit_change("org.gnome.gsettings", PAYLOAD)
 
         # Check change is in queue
@@ -74,23 +101,27 @@ class TestConnMgr(unittest.TestCase):
         self.assertEqual(mgr.timeout, 0)
 
         # Check data has been written to spiceport file
-        data = read_file(path)
+        raw_data = read_file(path)
+        actual = parse_fc_message(raw_data)
+        self.assertListEqual(actual, expected_data)
+        # check that the change is correct dumped JSON object
+        # dump it again for string comparison
         self.assertEqual(
-            json.dumps(json.loads(expected_data)), json.dumps(json.loads(data))
+            json.dumps(json.loads(actual[0])), json.dumps(json.loads(expected_data[0]))
         )
 
-    def test_02_manager_queue(self):
+    def test_03_manager_queue(self):
         # Get temporary file
         TMPFILE = Gio.file_new_tmp("fc_logger_spiceport_XXXXXX")
         path = TMPFILE[0].get_path()
 
         mgr = FleetCommander.SpicePortManager(path)
         PAYLOADS = ["1", "2", "3", "4", "5"]
-        expected_data = ""
+        expected_data = []
 
         for pl in PAYLOADS:
             mgr.submit_change("org.gnome.gsettings", pl)
-            expected_data += json.dumps({"ns": "org.gnome.gsettings", "data": pl})
+            expected_data.append(json.dumps({"ns": "org.gnome.gsettings", "data": pl}))
 
         self.assertEqual(len(PAYLOADS), len(mgr.queue))
 
@@ -106,8 +137,53 @@ class TestConnMgr(unittest.TestCase):
         self.assertEqual(mgr.timeout, 0)
 
         # Check data has been written to spiceport file
-        data = read_file(path)
-        self.assertEqual(expected_data, data)
+        raw_data = read_file(path)
+        actual = parse_fc_message(raw_data)
+        self.assertListEqual(actual, expected_data)
+
+    def test_04_big_message(self):
+        """Test logger sends a data in chunks."""
+        path = "/logger_notify_channel"
+        payload = "A" * 32768
+
+        with patch("builtins.open", mock_open(), create=True) as m:
+            mgr = FleetCommander.SpicePortManager(path)
+
+        m.assert_called_once_with(path, "wb", 0)
+        fd = m()
+        fd.write.assert_called_once_with(FC_LOGGER_PROTO_HEADER.encode())
+        fd.write.reset_mock()
+
+        mgr.submit_change("org.gnome.gsettings", payload)
+
+        # Check change is in queue
+        self.assertEqual(len(mgr.queue), 1)
+        self.assertEqual(mgr.queue[0]["ns"], "org.gnome.gsettings")
+        self.assertEqual(mgr.queue[0]["data"], payload)
+
+        # Clean queue and quit
+        mgr.give_up()
+        self.assertEqual(len(mgr.queue), 0)
+        self.assertEqual(mgr.timeout, 0)
+
+        # Check data has been written to the mocked file
+        expected_data = (
+            json.dumps({"ns": "org.gnome.gsettings", "data": payload})
+            + FC_LOGGER_PROTO_SUFFIX
+        ).encode()
+
+        expected_chunks_count = math.ceil(
+            len(expected_data) / FC_LOGGER_PROTO_CHUNK_SIZE
+        )
+        self.assertEqual(fd.write.call_count, expected_chunks_count)
+
+        actual_data = b""
+        for call_args in fd.write.call_args_list:
+            write_args, _ = call_args
+            self.assertEqual(len(write_args), 1)
+            actual_data += write_args[0]
+
+        self.assertEqual(actual_data, expected_data)
 
 
 if __name__ == "__main__":
